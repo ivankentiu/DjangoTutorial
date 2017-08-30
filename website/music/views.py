7c3320ae33d31bd15b478d6bb9fd36c3a630b6ba
Ivan Tiu <ivankentiu@gmail.com>
2017-08-30 13:20:56 +0800

    combine load and render

--- website/music/views.py
+++ website/music/views.py
@@ -1,9 +1,19 @@
 # from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
+#from  django.template import loader
 from .models import Album
 
-
+"""
+def index1(request):
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_albums
+    }
+    return HttpResponse(template.render(context, request))
+"""
+# Combines Load and Render Function
 def index(request):
     # return HttpResponse("<h1>This is the Music app homepage</h1>")
     all_albums = Album.objects.all()
